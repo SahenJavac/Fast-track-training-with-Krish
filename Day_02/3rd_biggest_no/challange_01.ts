eee0f2870e861b53e744ec4ffe688d5357a3d90a
SahenJavac <54188335+SahenJavac@users.noreply.github.com>
2021-05-29 01:00:54 +0530

    Delete Day_02 directory

--- Day_02/3rd_biggest_no/challange_01.ts
+++
@@ -1,9 +0,0 @@
-function findThirdBiggestNo(arr) {
-
-    arr.sort(function (a, b) { return a - b });
-    console.log(arr)
-    console.log('Third Biggest Number is - ' + arr[2])
-}
-
-var numArray = [12, 45, 6, 1, -34, 78, -6];
-findThirdBiggestNo(numArray);
